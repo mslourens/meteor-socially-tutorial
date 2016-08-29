bd63f0b31f78e2714a5885884698f41794906da8
Maurits Lourens <lourens_M@hotmail.com>
2016-08-29 19:46:46 +0200

    finished step-07 - folder structure

--- both/collections/parties.collection.ts
+++ both/collections/parties.collection.ts
@@ -1,3 +1,4 @@
 import {Mongo} from 'meteor/mongo';
+import {Party} from '../models/party.model';
 
-export const Parties = new Mongo.Collection('parties');
+export const Parties = new Mongo.Collection<Party>('parties');
--- server/imports/fixtures/parties.ts
+++ server/imports/fixtures/parties.ts
@@ -1,4 +1,5 @@
 import {Parties} from '../../../both/collections/parties.collection';
+import {Party} from '../../../both/models/party.model';
 
 export function loadParties() {
   if (Parties.find().count() === 0) {
